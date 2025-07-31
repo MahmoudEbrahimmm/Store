0839177a25d9ff01cf296cf42f82875ea9db96ef
mahmoudebrahim <mahmoudasar794@gmail.com>
2025-07-31 05:01:28 +0300

    create middleware last_user_active_at

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,6 @@ Route::get('/',[HomeConreoller::class,'index'])->name('home');
 Route::get('/products',[ProductsController::class,'index'])->name('products.index');
 Route::get('/products/{product:slug}',[ProductsController::class,'show'])->name('products.show');
 
+
 require __DIR__.'/auth.php';
 require __DIR__.'/dashboard.php';
